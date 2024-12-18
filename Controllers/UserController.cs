b0471709695241cd247216f05feb445f98b57481
github-cloudlabsuser-1283 <github_cloudlabsuser_1283@cloudlabsaiuser.com>
2024-12-18 07:45:15 +0000

    push

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Net.NetworkInformation;
 using Microsoft.AspNetCore.Mvc;
 using MyMvcApp.Models;
 
@@ -12,18 +13,27 @@ public class UserController : Controller
         public ActionResult Index()
         {
             // Implement the Index method here
+ 
+         return View(userlist);
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+    var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
         }
 
         // POST: User/Create
@@ -31,6 +41,19 @@ public class UserController : Controller
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+             try
+            {
+                if (ModelState.IsValid)
+                {
+                    userlist.Add(user);
+                    return RedirectToAction("Index");
+                }
+                return View(user);
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: User/Edit/5
@@ -38,6 +61,12 @@ public class UserController : Controller
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+       var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
         }
 
         // POST: User/Edit/5
@@ -49,12 +78,41 @@ public class UserController : Controller
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+        
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                    if (existingUser == null)
+                    {
+                        return NotFound();
+                    }
+                    existingUser.Name = user.Name;
+                    existingUser.Email = user.Email;
+                    //existingUser.Age = user.Age;
+                    return RedirectToAction("Index");
+                }
+                return View(user);
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+
+
         }
 
         // POST: User/Delete/5
@@ -62,5 +120,20 @@ public class UserController : Controller
         public ActionResult Delete(int id, IFormCollection collection)
         {
             // Implement the Delete method (POST) here
+            try
+            {
+                var user = userlist.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
+                userlist.Remove(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
 }
